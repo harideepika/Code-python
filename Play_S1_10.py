f47eceeb4fa9fb8ab1f6e1672230c2fddf4cfa56
harideepika <46592994+harideepika@users.noreply.github.com>
2019-03-24 12:53:41 +0530

    Create Play_S1_10.py

---
+++ Play_S1_10.py
@@ -0,0 +1,9 @@
+#hari
+import sys, string, math
+s1, s2 = input().split()
+k = 0
+for i in range(len(s1)) :
+    if s1[i] != s2[i] :
+        k += 1
+if k == 1 : print('yes')
+else :      print('no')
