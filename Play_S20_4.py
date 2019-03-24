693f9d9fb34f4bcb0b4a52114db85c033331900d
harideepika <46592994+harideepika@users.noreply.github.com>
2019-03-24 19:41:48 +0530

    Create Play_S20_4.py

---
+++ Play_S20_4.py
@@ -0,0 +1,16 @@
+#print d
+import sys, string, math
+
+s1,s2 = input().split()
+if s1 == 'P' and s2 == 'R' or s1 == 'R' and s2 == 'P' :
+    print('P')
+elif s1 == 'P' and s2 == 'S' or s1 == 'S' and s2 == 'P' :
+    print('S')
+elif s1 == 'S' and s2 == 'R' or s1 == 'R' and s2 == 'S' :
+    print('R')
+elif s1 == 'S' and s2 == 'S' :
+    print('D')
+elif s1 == 'P' and s2 == 'P' :
+    print('D')
+elif s1 == 'R' and s2 == 'R' :
+    print('D')
