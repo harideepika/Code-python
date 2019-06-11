0f95d83b7280b59062402941b9ab8fc25b832665
harideepika <46592994+harideepika@users.noreply.github.com>
2019-06-11 15:22:29 +0530

    Update Beg_s1_5.py

--- Beg_s1_5.py
+++ Beg_s1_5.py
@@ -1,6 +1,6 @@
 #largest of 3 nos py
 import sys
-a,b,c = map(int,(input('enter 3 nos : ').split()))
+a,b,c = map(int,(input().split()))
 #print(a,b,c)
 if a>b and a>c : print(a)
 elif b>c : print(b)
